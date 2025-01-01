93b7904c3261c44f709db18dbc119d5f6c098dce
Sofonyas <sofstzz@gmail.com>
2025-01-01 13:19:06 +0300

    build docker

---
+++ scripts/build_docker.sh
@@ -0,0 +1,7 @@
+
+
+# Build the Docker image
+docker build -t "docker-image"
+
+# Optional: Push the Docker image to a registry (e.g., Docker Hub, Google Container Registry)
+# docker push "docker-image"
